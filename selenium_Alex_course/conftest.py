b9325ce17137db8388ca4b0cdef3877e5c33ad82
Grom-Zadira85 <Trall18111985@gmail.com>
2024-10-25 19:15:42 +0300

    add allure, move file to dir selenium

--- selenium_Alex_course/conftest.py
+++ selenium_Alex_course/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+import allure
 
 
+@allure.title('Test')
 @pytest.fixture(scope="function")
 def browser():
+    options = ChromeOptions()
+    options.add_argument("--window-size=1440,920")
+    options.add_argument("--incognito")
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
     yield browser
