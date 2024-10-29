3c2bb16bf84080061cf9cd7055d10124253af720
Grom-Zadira85 <Trall18111985@gmail.com>
2024-10-29 20:03:47 +0200

    add test upload_image

--- Konstanta/Api/alex_course/Api_2.py
+++ Konstanta/Api/alex_course/Api_2.py
@@ -66,13 +66,34 @@ def test_add_photo_pet_upload_image():
         headers={
             "api_key": "special-key"
         },
-        files=("woolf".jpg, open("subfolder/woolf.jpg", "rb"))
-
+        files={
+            "file": ("woolf.jpg", open(r"D:\My project PY\selenium-school\Konstanta\Api\alex_course\woolf.jpg", "rb"),
+                     "image/jpg")
+        }
     )
+    assert response.status_code == 200, f"Expected status code 200, got {response.status_code}"
+
+    # Получаем JSON-ответ и проверяем, что он содержит сообщение про информацию о загруженном файле
+    assert "File uploaded to" in response.json().get("message", ""), "File upload confirmation message not found"
 
     print(response.status_code)
     print(response.json())
     formatted_response = json.dumps(response.json(), sort_keys=True, indent=4)
     print(formatted_response)
 
+    """                response_json.get("message", ""):
 
+response.json — это объект JSON, полученный из ответа сервера.
+.get("message", "") — метод .get() извлекает значение ключа "message" из JSON. 
+Если ключа "message" нет в ответе, по умолчанию будет возвращена пустая строка "".
+
+                 File uploaded to" in response_json.get("message", ""):
+Эта часть проверяет, что подстрока "File uploaded to" присутствует в значении "message". 
+Иными словами, если сообщение о загрузке файла присутствует, эта проверка вернется True.
+assert "File uploaded to" in response_json.get("message", ""), "File upload confirmation message not found":
+
+Если условие не выполняется (в данном случае, если подстрока 
+"File uploaded to" не найдена в "message"), assert выбрасывает ошибку и останавливает выполнение кода.
+
+Если ошибка возникает, выведется сообщение "File upload confirmation message not found" для отладки.
+В итоге  Эта строка проверяет, что в ответе содержится текст, подтверждающий, что файл был загружен."""
