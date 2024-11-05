8ee76fe644dfb93b30ea768316ad005041769141
Grom-Zadira85 <Trall18111985@gmail.com>
2024-11-05 10:49:53 +0200

    test_check box

--- selenium_Alex_course/demoqa.py
+++ selenium_Alex_course/demoqa.py
@@ -38,3 +38,72 @@ def test_text_box(browser):
 
     assert check_field.text == expected_text, f"Expected '{expected_text}', but got '{check_field.text}'"
     time.sleep(2)
+
+
+def test_check_box(browser):
+    browser.get("https://demoqa.com/checkbox")
+
+    check_icon = browser.find_element(By.CSS_SELECTOR, '[class="rct-icon rct-icon-uncheck"]')
+    check_icon.click()
+
+    toggle = browser.find_element(By.CSS_SELECTOR, '[class="rct-icon rct-icon-expand-close"]')
+    toggle.click()
+
+    # Найти все отмеченные чекбоксы
+    checked_icons = browser.find_elements(By.CSS_SELECTOR, '[class="rct-icon rct-icon-check"]')
+    time.sleep(2)
+
+    # Выведем статус чек-бокса True/False, так как он меняется при клике на элемент, отвечающий за выставление флажка
+    HOME_CHECKBOX = ("xpath", "//input[@id='tree-node-home']")
+    print(browser.find_element(*HOME_CHECKBOX).is_selected())
+
+    # Проверка, что хотя бы один чекбокс был отмечен
+    assert len(checked_icons) > 0, "Не удалось отметить чек-боксы"
+
+    time.sleep(2)
+
+""" 
+# assert CHECK_BOX.get_attribute("checked") is not None
+# assert CHECK_BOX.is_selected() is True
+
+В вашей функции test_check_box возникла ошибка из-за использования assert CHECK_BOX.is_selected().
+ Это происходит потому, что is_selected() работает только для элементов типа <input type="checkbox">, 
+ тогда как в данном случае CHECK_BOX — это не сам чекбокс, а иконка, которая визуально показывает его состояние."""
+
+"""
+Чтобы выбрать первый элемент с классом rct-icon rct-icon-expand-open, используя XPath, 
+можно воспользоваться следующим выражением:
+(//*[@class="rct-icon rct-icon-expand-open"])[1]
+
+
+====== Для доступа по порядковому номеру, необходимо использовать [n], все как в Python) Но есть пара нюансов!=======
+
+Допустим, у нас есть HTML-код и нам необходимо найти div с текстом 2:
+
+<root>
+    <header>
+        <div>1</div>
+        <div>2</div>
+        <section>
+            <div>3</div>
+            <div>4</div>
+        </section>
+    </header>
+</root>
+Казалось бы, если мы напишем такой запрос:
+ //header//div[2] то получим второй div внутри header, как раз который нам нужен, но нет! 
+ Мы получим "Каждый второй div лежащий внутри header"
+
+Такой способ в изучаемом нами подходе не будет работать и врятли пригодиться, ибо нам вернуться 2 div блока, 
+а нам нужно немного другое!
+
+--------------------Мы напишем следующий Xpath: (//header//div)[2]  --------------------------------------------
+
+Разобьем по частям, чтобы было понятнее:
+1. (//header//div) - как вы видите стоят скобки, они вернут нам список всех div-блоков внутри header.
+Пример для наглядности: (<div>1</div>, <div>2</div>, <div>3</div>, <div>4</div>)
+
+2. (//header//div)[2] - тут мы получаем второй div из полученного списка и все работает)
+Важно: индексация элементов в Xpath идет не с 0 как в Python, а с 1
+=======================================================================================================================
+"""
