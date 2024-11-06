156409f79c8429ba2e913ba92083fea33433cdab
Grom-Zadira85 <Trall18111985@gmail.com>
2024-11-06 09:38:39 +0200

    refactor

--- selenium_Alex_course/conftest.py
+++ selenium_Alex_course/conftest.py
@@ -1,17 +1,24 @@
+import allure
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-import allure
+
+
+@pytest.fixture
+def chrome_options():
+    options = ChromeOptions()
+    # options.add_argument("--window-size=1440,920")
+    options.add_argument("--incognito")
+    options.add_argument("--disable-cache")
+    options.add_argument('--start-maximized')
+    return options
 
 
 @allure.title('Test')
 @pytest.fixture(scope="function")
-def browser():
-    options = ChromeOptions()
-    # options.add_argument("--window-size=1440,920")
-    options.add_argument("--incognito")
+def browser(chrome_options):
     print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+    browser = webdriver.Chrome(options=chrome_options)
     yield browser
     print("\nquit browser..")
     browser.quit()
