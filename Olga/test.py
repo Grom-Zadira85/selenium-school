5e58f8e884fea70b02587c2c843a84a20b4253af
OliqSh <olga263368@gmail.com>
2024-11-19 14:31:46 +0500

    test drop_down (#2)

--- Olga/test.py
+++ Olga/test.py
@@ -1,1 +1,34 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+url = "http://the-internet.herokuapp.com/dropdown"
+
+
+def test_drop_down(browser):
+    browser.get(url)
+    browser.find_element(By.CSS_SELECTOR, '[id="dropdown"]').click()
+    time.sleep(3)
+
+    value1 = browser.find_element(By.CSS_SELECTOR, '[value = "1"]')
+    value1.click()
+    time.sleep(3)
+    assert value1.text == "Option 1"
+
+    value2 = browser.find_element(By.CSS_SELECTOR, '[value = "2"]')
+    value2.click()
+    time.sleep(3)
+
+    assert value2.text == "Option 2"
+
+
+
+
+
+
+
+
+
+
+
 
