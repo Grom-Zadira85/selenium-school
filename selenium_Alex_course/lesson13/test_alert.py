8ad519f5a888bad448cc88685d50c4cf98a0c04d
Konstantin <161002599+Grom-Zadira85@users.noreply.github.com>
2024-11-26 17:40:01 +0200

    Update test_alert.py (#21)

--- selenium_Alex_course/lesson13/test_alert.py
+++ selenium_Alex_course/lesson13/test_alert.py
@@ -5,10 +5,7 @@ from .page_alert import BasePage, url
 
 
 def test_alert(browser):
-    page = BasePage(browser, url)
-    page.open_page()
-    # page.click_button_to_see_alert()
-    # page.click_alert_after_5_seconds()
-    # page.confirm_box_wll_appear()
-    page.prompt_box_will_appear()
+    #page = BasePage(browser, url)
+    #page.open_page()
+    #page.prompt_box_will_appear()
     time.sleep(2)
