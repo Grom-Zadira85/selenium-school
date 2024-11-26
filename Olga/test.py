91c827d3ccd3363892097512391ffc3f131161ae
Oliq <olga263368@gmail.com>
2024-11-26 10:31:27 +0100

    test drop_down + dd

--- Olga/test.py
+++ Olga/test.py
@@ -1,7 +1,3 @@
-import time
-
-from selenium.webdriver.common.by import By
-
 url = "http://the-internet.herokuapp.com/dropdown"
 
 
@@ -24,6 +20,33 @@ def test_drop_down(browser):
 
 
 
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def test_drop_down2(browser):
+    # Open the target URL
+    browser.get("https://demoqa.com/select-menu")
+
+    # Locate and click the dropdown to open it
+    dropdown = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '(//*[@class=" css-1hwfws3"])[1]'))
+    )
+    dropdown.click()
+
+    # Wait for the options to load and locate the desired option
+    option = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[text()='Group 2, option 2']"))
+    )
+
+    # Click the desired option
+    option.click()
+
+    # Add a short sleep for observation (optional)
+    time.sleep(3)
 
 
 
