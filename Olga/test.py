4e84c1c5bb64bb44fb66780950089d270cc40f2b
Oliq <olga263368@gmail.com>
2024-11-27 10:35:55 +0100

    add dd3_test

--- Olga/test.py
+++ Olga/test.py
@@ -1,10 +1,11 @@
 import time
 
-from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+
 
 url = "http://the-internet.herokuapp.com/dropdown"
 
@@ -62,6 +63,27 @@ def test_drop_down2(browser):
 
     # Еще небольшая пауза для проверки (опционально)
     time.sleep(2)
+
+
+def test_drop_down3(browser):
+    browser.get("https://demoqa.com/select-menu")
+    # browser.find_element(By.CSS_SELECTOR, '[id="oldSelectMenu"]')
+    browser.find_element(By.CSS_SELECTOR, '[value="2"]').click()
+    time.sleep(3)
+    MS = browser.find_element(By.CSS_SELECTOR, '[id="react-select-4-input"]')
+    MS.send_keys("Gre")
+    time.sleep(3)
+    assert MS.get_attribute("value") == "Gre"
+    MS.send_keys(Keys.ENTER)
+    time.sleep(3)
+
+    MS2 = browser.find_element(By.CSS_SELECTOR, '[value="audi"]')
+    browser.execute_script("arguments[0].scrollIntoView();", MS2)
+    browser.find_element(By.CSS_SELECTOR, '[value="audi"]').click()
+    time.sleep(6)
+    assert MS2.get_attribute("value") == "audi"
+
+    time.sleep(3)
 
 
 
